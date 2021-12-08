12a34d8e13f0014ba743c0c7b438d678748a5516
Snicewicz <snicewicz@gmail.com>
2021-12-08 09:56:32 -0500

    commit tree c6b721067cd6fb31b2ac7782499c1a7d9bbd7f32

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
 
+def my_collect(array)
+    i = 0
+    while i < array.length
+        yeild(array[i])
+        i =+ 1
+    end
+end
 
+my_collect(array) do |name|
+name.split(" ").first
+end
